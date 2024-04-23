2692f8fee9ed6a23deef0ebfe9f40d57996aa354
Gabriel Vargas <gabriel.vargas.coelho@gmail.com>
2024-04-22 22:00:35 -0300

    feature: add capacidade de iterações para o teste de pi

--- app/pi_test/pi_test.cc
+++ app/pi_test/pi_test.cc
@@ -21,12 +21,12 @@ long max(unsigned int a, unsigned int b, unsigned int c) { return ((a >= b) && (
 
 OStream cout;
 Chronometer chrono;
-Periodic_Thread * thread_l; // low
-Periodic_Thread * thread_m; // medium
-Periodic_Thread * thread_h; // high
+Periodic_Thread *thread_l; // low
+Periodic_Thread *thread_m; // medium
+Periodic_Thread *thread_h; // high
 
-Mutex * mutex1;
-Mutex * mutex2;
+Mutex *mutex1;
+Mutex *mutex2;
 
 int main()
 {
@@ -51,57 +51,73 @@ int main()
 
 int func_l()
 {
-    cout << "Executing low p(l) = " << thread_l->priority() << endl;
-    cout << "Low tries to get mutex 1" << endl;
-    mutex1->lock();
-    cout << "Low gets mutex 1" << endl;
-    cout << "Executing low inside mutex 1 p(l) = " << thread_l->priority() << endl;
-    cout << "Low tries to get mutex 2" << endl;
-    mutex2->lock();
-    cout << "Low gets mutex 2" << endl;
-    if (!thread_m) {
-        cout << "Low creates thread Medium " << endl;
-        thread_m = new Periodic_Thread(RTConf(period_m * 1000, 0, 0, 0, iterations, wcet_m * 1000), &func_m);
-    }
-    cout << "Executing low inside mutex 2 p(l) = " << thread_l->priority() << endl;
-    if (!thread_h) {
-        cout << "Low creates thread High " << endl;
-        thread_h = new Periodic_Thread(RTConf(period_h * 1000, 0, 0, 0, iterations, wcet_h * 1000), &func_h);
-    }
-    cout << "Executing low inside mutex 2 p(l) = " << thread_l->priority() << endl;
-    cout << "Low releases mutex 2" << endl;
-    mutex2->unlock();
-    cout << "Executing low inside mutex 1 p(l) = " << thread_l->priority() << endl;
-    cout << "Low releases mutex 1" << endl;
-    mutex1->unlock();
-    cout << "Executing low p(l) = " << thread_l->priority() << endl;
-    cout << "Low done" << endl;
+    do{
+        cout << "Executing low p(l) = " << thread_l->priority() << endl;
+        cout << "Low tries to get mutex 1" << endl;
+        mutex1->lock();
+        cout << "Low gets mutex 1" << endl;
+        cout << "Executing low inside mutex 1 p(l) = " << thread_l->priority() << endl;
+        cout << "Low tries to get mutex 2" << endl;
+        mutex2->lock();
+        cout << "Low gets mutex 2" << endl;
+        if (!thread_m)
+        {
+            cout << "Low creates thread Medium" << endl;
+            thread_m = new Periodic_Thread(RTConf(period_m * 1000, 0, 0, 0, iterations, wcet_m * 1000), &func_m);
+        }
+        cout << "Executing low inside mutex 2 p(l) = " << thread_l->priority() << endl;
+        if (!thread_h)
+        {
+            cout << "Low creates thread High " << endl;
+            thread_h = new Periodic_Thread(RTConf(period_h * 1000, 0, 0, 0, iterations, wcet_h * 1000), &func_h);
+        }
+        cout << "Executing low inside mutex 2 p(l) = " << thread_l->priority() << endl;
+        cout << "Low releases mutex 2" << endl;
+        mutex2->unlock();
+        cout << "Executing low inside mutex 1 p(l) = " << thread_l->priority() << endl;
+        cout << "Low releases mutex 1" << endl;
+        mutex1->unlock();
+        cout << "Executing low p(l) = " << thread_l->priority() << endl;
+        cout << "Low job done" << endl;
+    } while (Periodic_Thread::wait_next());
+
+    cout << "Low thread done" << endl;
 
     return 0;
 }
 
 int func_m()
 {
-    cout << "Executing medium" << endl;
-    cout << "Medium tries to get mutex 1" << endl;
-    mutex1->lock();
-    cout << "Medium gets mutex 1" << endl;
-    cout << "Executing medium inside mutex 1" << endl;
-    cout << "Medium releases mutex 1" << endl;
-    mutex1->unlock();
-    cout << "Medium done " << endl;
+    do {
+        cout << "Executing medium" << endl;
+        cout << "Medium tries to get mutex 1" << endl;
+        mutex1->lock();
+        cout << "Medium gets mutex 1" << endl;
+        cout << "Executing medium inside mutex 1" << endl;
+        cout << "Medium releases mutex 1" << endl;
+        mutex1->unlock();
+        cout << "Medium job done" << endl;
+    } while (Periodic_Thread::wait_next());
+
+    cout << "Medium thread done" << endl;
+
     return 0;
 }
 
 int func_h()
 {
-    cout << "Executing high" << endl;
-    cout << "High tries to get mutex 2" << endl;
-    mutex2->lock();
-    cout << "High gets mutex 2" << endl;
-    cout << "Executing high inside mutex 2" << endl;
-    cout << "High releases mutex 2" << endl;
-    mutex2->unlock();
-    cout << "High done " << endl;
+    do {
+        cout << "Executing high" << endl;
+        cout << "High tries to get mutex 2" << endl;
+        mutex2->lock();
+        cout << "High gets mutex 2" << endl;
+        cout << "Executing high inside mutex 2" << endl;
+        cout << "High releases mutex 2" << endl;
+        mutex2->unlock();
+        cout << "High job done" << endl;
+    } while (Periodic_Thread::wait_next());
+
+    cout << "High thread done" << endl;
+
     return 0;
 }
