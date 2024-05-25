3142c6cc0646de4d7808905900b0d5a15b50e684
Gabriel Vargas <gabriel.vargas.coelho@gmail.com>
2024-05-24 22:02:39 -0300

    feature: teste de mudança de prioridade

--- app/multicore_priority_test/multicore_priority_test.cc
+++ app/multicore_priority_test/multicore_priority_test.cc
@@ -25,18 +25,19 @@ long max(unsigned int a, unsigned int b, unsigned int c) { return ((a >= b) && (
 OStream cout;
 Chronometer chrono;
 Periodic_Thread * thread[3];
-Periodic_Thread * thread_l; // low0
-Periodic_Thread * thread_m; // low1
-Periodic_Thread * thread_h; // low2
+Periodic_Thread * thread_l; // low
+Periodic_Thread * thread_m; // medium
+Periodic_Thread * thread_h; // high
 
 int main()
 {
+    cout << "O teste foi idealizado para funcionar com 3 cores e usando o PLLF" << endl;
     cout << "Testando mudança de prioridade no multicore..." << endl;
     cout << "A thread low vai ter sua prioridade e também sua fila mudada durante várias ocasiões." << endl;
 
-    thread_l = new Periodic_Thread(RTConf(period_l * 1000, 0, 0, 0, iterations, wcet_l * 1000), &func_l);
-    thread_m = new Periodic_Thread(RTConf(period_m * 1000, 0, 0, 0, iterations, wcet_m * 1000), &func_m);
-    thread_h = new Periodic_Thread(RTConf(period_h * 1000, 0, 0, 0, iterations, wcet_h * 1000), &func_h);
+    thread_l = new Periodic_Thread(RTConf(period_l * 1000, 0, 0, 0, iterations, wcet_l * 1000), &func_l); // queue 1
+    thread_m = new Periodic_Thread(RTConf(period_m * 1000, 0, 0, 0, iterations, wcet_m * 1000), &func_m); // queue 2
+    thread_h = new Periodic_Thread(RTConf(period_h * 1000, 0, 0, 0, iterations, wcet_h * 1000), &func_h); // queue 0
     for (unsigned int i = 0; i < 3; i++)
         thread[i] = new Periodic_Thread(RTConf((period_m + 100 )* 1000, 0, 0, 0, iterations, wcet_m * 1000), &func);
     chrono.start();
@@ -58,82 +59,82 @@ int main()
 
 int func_l()
 {
-    do {
-        cout << "Executing low" << endl;
+    cout << "Executing low" << endl;
 
-        Microsecond elapsed = chrono.read() / 1000;
-        for(Microsecond end = elapsed + for_time + 500, last = end; end > elapsed; elapsed = chrono.read() / 1000) {
-            if(last != elapsed) {
-                if (elapsed%3 == 0)
-                    cout << "Executing low p(l) = " << thread_l->priority() << endl;
-                last = elapsed;
-            }
+    Microsecond elapsed = chrono.read() / 1000;
+    for(Microsecond end = elapsed + for_time + 500, last = end; end > elapsed; elapsed = chrono.read() / 1000) {
+        if(last != elapsed) {
+            if (elapsed%3 == 0)
+                cout << "Executing low p(l) = " << thread_l->priority() << endl;
+            last = elapsed;
         }
-        
-        cout << "Low done" << endl;
-    } while (Periodic_Thread::wait_next());
+    }
+    
+    cout << "Low done" << endl;
     cout << "Low finish" << endl;
     return 0;
 }
 
 int func_m()
 {
-    do {
-        cout << "Executing medium" << endl;
+    cout << "Executing medium" << endl;
 
-        Microsecond elapsed = chrono.read() / 1000;
-        for(Microsecond end = elapsed + for_time + 500, last = end; end > elapsed; elapsed = chrono.read() / 1000) {
-            if(last != elapsed) {
-                if (elapsed%5 == 0)
-                    cout << "Executing medium p(l) = " << thread_m->priority() << endl;
-                last = elapsed;
-            }
+    Microsecond elapsed = chrono.read() / 1000;
+    for(Microsecond end = elapsed + for_time + 500, last = end; end > elapsed; elapsed = chrono.read() / 1000) {
+        if(last != elapsed) {
+            if (elapsed%5 == 0)
+                cout << "Executing medium p(l) = " << thread_m->priority() << endl;
+            last = elapsed;
         }
+    }
        
-    } while (Periodic_Thread::wait_next());
     cout << "Medium finish" << endl;
     return 0;
 }
 
 int func_h()
 {
-    do {
-        cout << "Executing High" << endl;
+    cout << "Executing High" << endl;
 
-        auto cl = LLF(1000000, 1000000, 0, 0, 50000);
-        cl.set_queue(2);
-        thread_l->priority(cl);
+    cout << "Changing thread low to core 2" << endl;
+    auto cl = PLLF(1000000, 1000000, 0, 0, 50000);
+    cl.set_queue(2);
+    thread_l->priority(cl);
 
-        Machine::delay(50000);
+    Machine::delay(50000);
 
-        auto cm = LLF(1000000, 1000000, 0, 0, 300000);
-        cm.set_queue(2);
-        thread_m->priority(cm);
+    cout << "Changing priority of thread medium" << endl;
+    auto cm = PLLF(1000000, 1000000, 0, 0, 300000);
+    cm.set_queue(2);
+    thread_m->priority(cm);
 
-        Microsecond elapsed = chrono.read() / 1000;
-        for(Microsecond end = elapsed + for_time, last = end; end > elapsed; elapsed = chrono.read() / 1000) {
-            if(last != elapsed) {
-                if (elapsed%7 == 0)
-                    cout << "Executing high p(l) = " << thread_h->priority() << endl;
-                last = elapsed;
-            }
+    Machine::delay(50000);
+
+    cout << "Changing thread low to core 0" << endl;
+    cout << "Thread high will be replaced by thread low" << endl;
+    auto cl2 = PLLF(1000000, 1000000, 0, 0, 300000);
+    thread_l->priority(cl2);
+
+    Microsecond elapsed = chrono.read() / 1000;
+    for(Microsecond end = elapsed + for_time, last = end; end > elapsed; elapsed = chrono.read() / 1000) {
+        if(last != elapsed) {
+            if (elapsed%7 == 0)
+                cout << "Executing high p(l) = " << thread_h->priority() << endl;
+            last = elapsed;
         }
+    }
 
-
-    } while (Periodic_Thread::wait_next());
     cout << "High finish" << endl;
     return 0;
 }
 
 int func()
 {
-    do {
-        Microsecond elapsed = chrono.read() / 1000;
-        for(Microsecond end = elapsed + for_time, last = end; end > elapsed; elapsed = chrono.read() / 1000)
-            if(last != elapsed) {
-                last = elapsed;
-            }
-       
-    } while (Periodic_Thread::wait_next());
+    Microsecond elapsed = chrono.read() / 1000;
+    for(Microsecond end = elapsed + for_time, last = end; end > elapsed; elapsed = chrono.read() / 1000)
+        if(last != elapsed) {
+            last = elapsed;
+        }
+    
     return 0;
 }
